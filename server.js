f01b3e14eb07667e3b58cdac578cecda6b15d74a
Harry Winer <winer.harry@gmail.com>
2021-07-31 17:52:35 +0100

    added api to server

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const app = express();
 const port = process.env.PORT || 5000;
 app.use(express.static(path.join(__dirname, 'build')));
 
+app.use('/api', require('./api/api'))
+
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
